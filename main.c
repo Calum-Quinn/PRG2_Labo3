9d8e2edb053ec9821fd4ad21b46cd3106a329248
dariovasq <dario.vasques@heig-vd.ch>
2023-05-25 20:41:09 +0200

    main

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : main.c
+ Auteur(s)      : Ewan Mariaux, Calum Quinn, Dario Vasques
+ Date creation  : 25.05.2023
+
+ Description    : Programme servant à ...
+
+ Remarque(s)    : -
+
+ Compilateurs   : Apple clang version 14.0.3 (clang-1403.0.22.14.1) (Dario)
+                : Mingw-w64 g++ 12.2.0 (Calum et Ewan)
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+	return EXIT_SUCCESS;
+}
